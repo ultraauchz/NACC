b9cbfed882a9c6acd7a9f8b400b128bd3305ba5d
movalen <pakcapon@gmail.com>
2015-10-06 19:05:34 +0700

    sit update

--- modules/contract/form.php
+++ modules/contract/form.php
@@ -19,7 +19,7 @@
 						      <input type="text" class="form-control" placeholder="เลขประจำตัวผู้เสียภาษี/เลขประจำตัวประชาชน" id="" name="" />
 						    </div>
 						    <div class="col-sm-2">
-						    	<button type="button" class='btn btn-inverse'>ค้นหา</button>
+						    	<a href="#div_counterparty" class='btn btn-inverse modal_fade'>ค้นหา</a>
 						    </div>
 						</div>
 
@@ -208,6 +208,88 @@
 	</div>
 </div>
 
+<div style="display:none;">
+	<div id="div_counterparty">
+		<div style='padding:20px; padding-top:10px; background:#eee; margin:10px 0; border:solid 1px #aaa; margin-bottom:25px;'>
+			<div style='font-weight:bold;'>ค้นหา</div>
+			<form class="form-horizontal" action="#" method="post">
+				<div class="div_index_search col-sm-12">
+					<div class="col-sm-3" style="line-height: 1;">เลขประจาตัวผู้เสียภาษี/เลขประจำตัวประชาชน : </div> 
+					<div class="col-sm-3">
+						<input type='text' placeholder='เลขประจาตัวผู้เสียภาษี' class='form-control' style="display:inline-block; width:300px;">
+					</div>
+				</div>
+				<div class="div_index_search col-sm-12">
+					<div class="col-sm-3">ชื่อผู้ประกอบการ : </div>
+					<div class="col-sm-4">
+						<input type='text' placeholder='เลขประจาตัวผู้เสียภาษี' class='form-control' style="display:inline-block;">
+					</div>
+				</div>
+
+				<div class="div_btn_search">
+					<button type="button" class='btn btn-inverse'>ค้นหา</button>
+				</div>
+			</form>
+		</div>
+
+		<div class="text-center">
+			<div class="pagination">
+				<div class="pagination_data">มีทั้งหมด  159 รายการ   / 16 หน้า </div>
+				<span class="disabled">« Previous</span>
+				<span class="current">1</span>
+				<a href="#">2</a>
+				<a href="#">3</a>
+				<a href="#">4</a>
+				<a href="#">5</a>
+				<a href="#">6</a>
+				...
+				<a href="#">15</a>
+				<a href="#">16</a>
+				<a href="#" class="next">Next »</a>
+			</div>
+		</div>
+		
+		<div class='tableLayout list'>
+			<div class='header'>
+				<div class="text-center" style="width:50px;">ลำดับ</div>
+				<div>เลขประจำตัวผู้เสียภาษี/เลขประจำตัวประชาชน</div>
+				<div>ชื่อผู้ประกอบการ</div>
+				<div>ที่อยู่</div>
+				<div>เลือก</div>
+			</div>
+
+
+			<?php 
+				$dataList = array(
+					array('1100701061554', 'ชื่อคู่สัญญา4 นามสมมุติ', 'กทม.', 4),
+					array('1100705648690', 'ชื่อคู่สัญญา3 นามสมมุติ', 'กทม.', 3),
+					array('1100705001221', 'ชื่อคู่สัญญา2 นามสมมุติ', 'นนทบุรี', 2),
+					array('1055801061554', 'ชื่อคู่สัญญา1 นามสมมุติ', 'ปทุมธานี ', 1),
+					array('', '', '', 10),
+					array('', '', '', 9),
+					array('', '', '', 8),
+					array('', '', '', 7),
+					array('', '', '', 6),
+					array('', '', '', 5),
+				);
+
+				foreach($dataList as $item) {
+					echo '<div>';
+						echo '<div class="text-center">'.((empty($no))?$no = 1:++$no).'</div>';
+						echo '<div>'.$item[0].'</div>';
+						echo '<div>'.$item[1].'</div>';
+						echo '<div>'.$item[2].'</div>';
+						echo '<div>';
+							echo '<button class="btn btn-sm btn-blue btn-select-cp">เลือก</button>';
+						echo '</div>';
+					echo '</div>';
+				}
+			?>
+					
+		</div> <!-- <div class='tableLayout list'> -->
+	</div>
+</div>
+
 <script language='javascript'>
 	$(function(){
 		$('[name=date_start]').on('change', function(){
@@ -218,6 +300,10 @@
 		$('[name=date_end]').on('change', function(){
 			date_end = $('[name=date_end]').val();
 			datepicker_($('[name=date_start]'), $('[name=date_start]').val(), $('[name=date_end]').val());
+		});
+
+		$('.btn-select-cp').on('click', function() {
+			$('#cboxClose').click();
 		});
 	});
 </script>
